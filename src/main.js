fa1959ce3c0d00504f329bd1a54820740dccedf1
Lindsay Schnell <lschnell8@gmail.com>
2021-05-20 19:12:07 -0600

    Add functionality that allows user to create their own poster

--- src/main.js
+++ src/main.js
@@ -10,6 +10,10 @@ var showSavedPostersBtn = document.querySelector(".show-saved");
 var savedPostersPage = document.querySelector(".saved-posters");
 var takeMeBackBtn = document.querySelector(".show-main");
 var backToMainBtn = document.querySelector(".back-to-main");
+var showMyPosterBtn = document.querySelector(".make-poster");
+var titleInput = document.getElementById('poster-title');
+var imageInput = document.getElementById('poster-image-url');
+var quoteInput = document.getElementById('poster-quote')
 
 // event listeners go here 👇
 window.addEventListener("load", generatePoster);
@@ -18,6 +22,7 @@ makeYourOwnPosterBtn.addEventListener("click", showFormPage);
 showSavedPostersBtn.addEventListener("click", showSavedPosterPage)
 takeMeBackBtn.addEventListener("click", takeMeBack);
 backToMainBtn.addEventListener("click", backToMain);
+showMyPosterBtn.addEventListener("click", createUserPoster)
 // functions and event handlers go here 👇
 // (we've provided one for you to get you started)!
 function getRandomIndex(array) {
@@ -48,4 +53,16 @@ function takeMeBack() {
 function backToMain(){
   mainPage.classList.remove("hidden");
   savedPostersPage.classList.add("hidden");
-}
+};
+
+function createUserPoster() {
+  event.preventDefault();
+  formPage.classList.add("hidden");
+  mainPage.classList.remove("hidden");
+  images.push(imageInput.value);
+  titles.push(titleInput.value);
+  quotes.push(quoteInput.value);
+  image.src = imageInput.value;
+  title.innerText = titleInput.value;
+  quote.innerText = quoteInput.value;
+};
